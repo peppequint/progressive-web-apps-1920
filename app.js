2598d2d3a696d19cef57e6466493fa7e1ccb0896
Peppe-Quint <peppequintb@gmail.com>
2020-03-09 13:16:54 +0100

    init ejs templating

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public/views'));
+
+app.get('/', (req, res) => res.render('pages/index'));
 
 app.listen(port, () => console.log(`Progressive Web App running on port ${port}.`));
