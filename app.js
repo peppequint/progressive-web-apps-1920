c089e14acbde1f20e9fba1a2f37f115c24880285
Peppe-Quint <peppequintb@gmail.com>
2020-03-09 17:00:58 +0100

    fetch data api

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
+const fetch = require('node-fetch');
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,6 +12,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public/views'));
 
-app.get('/', (req, res) => res.render('pages/index'));
+app.get('/', (req, res) => {
+  fetch('https://api.football-data.org/v2/matches', {
+    headers: { 'X-Auth-Token': process.env.API_KEY }
+  })
+    .then(res => {
+      return res.json();
+    })
+    .then(data => console.log(data.matches[1].competition));
+});
 
 app.listen(port, () => console.log(`Progressive Web App running on port ${port}.`));
