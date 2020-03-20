32307f17b3477eebb1171225729fb94d424d1710
Peppe-Quint <peppequintb@gmail.com>
2020-03-20 13:04:11 +0100

    manifest icons

--- public/scripts/build-static.js
+++ public/scripts/build-static.js
@@ -2,5 +2,5 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 
 return gulp
-.src(['./public/src/manifest.json','./public/src/service-worker.js'])
+.src(['./public/src/manifest.json','./public/src/service-worker.js', './public/src/img/**/*.*'])
 .pipe(gulp.dest('./public/static/'));
