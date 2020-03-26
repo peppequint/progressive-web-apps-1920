652cf3c2e43d59d2733a05d9b48d3a4763750893
Peppe-Quint <peppequintb@gmail.com>
2020-03-26 16:43:36 +0100

    routes app

--- routes/routes.js
+++ routes/routes.js
@@ -1,45 +1,19 @@
 const express = require('express');
 const app = express();
 
-const data = require('./../modules/data');
+const overview = require('./../modules/overview');
+const match = require('./../modules/match');
+const competition = require('./../modules/competition');
 
 app
-  .get('/', (req, res) => {
-    const matches = data('https://api.football-data.org/v2/matches');
+  .get('/', overview)
 
-    const competitions = data('https://api.football-data.org/v2/competitions?plan=TIER_ONE');
+  .get('/match/:id', match)
 
-    Promise.all([matches, competitions]).then(data => {
-      console.log(data[0]);
-
-      res.render('pages/index', { matches: data[0].matches, competitions: data[1].competitions });
-    });
-  })
-
-  .get('/match/:id', (req, res) => {
-    const matchDetails = data(`https://api.football-data.org/v2/matches/${req.params.id}`);
-    matchDetails.then(data => {
-      const homeTeam = data.match.homeTeam.id;
-      const awayTeam = data.match.awayTeam.id;
-      const match = data;
-
-      const home = data(`https://api.football-data.org/v2/teams/${homeTeam}`);
-      const away = data(`https://api.football-data.org/v2/teams/${awayTeam}`);
-
-      Promise.all([home, away, match]).then(data => {
-        res.render('pages/match', { home: data[0], away: data[1], match: data[2] });
-      });
-    });
-  })
-
-  .get('/competition/:id', (req, res) => {
-    data(`https://api.football-data.org/v2/competitions/${req.params.id}/standings`).then(data => {
-      res.render('pages/competition', { competition: data });
-    });
-  })
+  .get('/competition/:id', competition)
 
   .get('/offline', (req, res) => {
     res.render('pages/offline');
   });
 
-  module.exports = app;
+module.exports = app;
