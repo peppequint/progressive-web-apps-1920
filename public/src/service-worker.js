f8c2819721b992bcd908e82e55c4f25a1acbb888
Peppe-Quint <peppequintb@gmail.com>
2020-03-26 10:09:26 +0100

    sw issue

--- public/src/service-worker.js
+++ public/src/service-worker.js
@@ -1,5 +1,8 @@
-const CORE_CACHE_VERSION = 'v1';
-const CORE_ASSETS = ['/index.css'];
+import fetch from 'node-fetch';
+import { response } from 'express';
+
+const CORE_CACHE_VERSION = 'cache-storage';
+const CORE_ASSETS = ['/index.css', '/offline', '/manifest.json'];
 
 self.addEventListener('install', e => {
   console.log('Installing sw.');
@@ -19,6 +22,34 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   console.log('Fetching sw.');
+  const request = e.request;
 
-  // e.respondWith(caches.open(CORE_CACHE_VERSION).then(cache => console.log(e.request.url)));
+  if (request.method === 'GET' && CORE_ASSETS.includes(pathName(request.url))) {
+    e.respondWith(
+      caches
+        .open(CORE_CACHE_VERSION)
+        .then(cache => cache.match(e.request.url))
+        .then(res => (res ? res : cloneReq(e.request, 'html-cache')))
+        .catch(e => {
+          return caches.open(CORE_CACHE_VERSION).then(cache => cache.match('/offline'));
+        })
+    );
+  }
 });
+
+function cloneReq(req, cache) {
+  return fetch(req).then(res => {
+    if (!res.ok) {
+      throw new TypeError('Bad response');
+    }
+
+    const clone = response.clone();
+    caches.open(cache).then(cache => cache.put(req, clone));
+    return response;
+  });
+}
+
+function pathName(request) {
+  const url = new URL(request);
+  return url.pathname;
+}
