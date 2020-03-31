7e16a9a708c3c11d0a28bd8920d90769234aa939
Peppe-Quint <peppequintb@gmail.com>
2020-03-31 11:50:26 +0200

    clean css

--- public/scripts/build-css.js
+++ public/scripts/build-css.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const clean = require('gulp-clean-css');
 
 return gulp
   .src('./public/src/css/*.css')
   .pipe(concat('index.css'))
+  .pipe(clean())
   .pipe(gulp.dest('./public/static/'));
