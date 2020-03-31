3557de872121ab77e738351c75abaa5d2ab2d758
Peppe-Quint <peppequintb@gmail.com>
2020-03-31 10:57:09 +0200

    small refactor

--- modules/match.js
+++ modules/match.js
@@ -1,19 +1,14 @@
+require('dotenv').config();
 const data = require('./../modules/data');
 
-function getHomeTeam(id) {
-  const home = data(`https://api.football-data.org/v2/teams/${id}`);
+function getDetailsTeam(id) {
+  const team = data(`${process.env.ENDPOINT}teams/${id}`);
 
-  return home;
-}
-
-function getAwayTeam(id) {
-  const away = data(`https://api.football-data.org/v2/teams/${id}`);
-
-  return away;
+  return team;
 }
 
 function getDetailsMatch(id) {
-  const match = data(`https://api.football-data.org/v2/matches/${id}`);
+  const match = data(`${process.env.ENDPOINT}matches/${id}`);
 
   return match;
 }
@@ -21,8 +16,8 @@ function getDetailsMatch(id) {
 async function match(req, res) {
   const data = await getDetailsMatch(req.params.id);
 
-  const homeTeam = await getHomeTeam(data.match.homeTeam.id);
-  const awayTeam = await getAwayTeam(data.match.awayTeam.id);
+  const homeTeam = await getDetailsTeam(data.match.homeTeam.id);
+  const awayTeam = await getDetailsTeam(data.match.awayTeam.id);
 
   return Promise.all([homeTeam, awayTeam, data]).then(data => {
     res.render('pages/match', { home: data[0], away: data[1], match: data[2] });
