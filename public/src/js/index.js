b691e9721f924ce42ea0cf3019bc9d601031f36b
Peppe-Quint <peppequintb@gmail.com>
2020-03-31 11:50:12 +0200

    config time

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,1 +1,8 @@
 console.log('index.js');
+
+let time = document.querySelector('.match-venue--time').innerHTML;
+let editedTime = time.substr(0, time.indexOf(':00 GMT+'));
+
+console.log(editedTime);
+
+document.querySelector('.match-venue--time').textContent = editedTime;
